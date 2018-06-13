938e2ccb2dfe49ac26fd7a154739808ea1f60d13
Adeline <adeline.contact@gmail.com>
2018-06-13 16:55:40 +0200

    ex1 js base2

---
+++ exercice1.js
@@ -0,0 +1,8 @@
+document.getElementById('image1').onmouseover = function () 
+    {
+        document.getElementById('image1').setAttribute("src","images/image1_2.jpg");
+    }
+document.getElementById('image1').onmouseout = function () 
+    {
+        document.getElementById('image1').setAttribute("src","images/image1.jpg");
+    }

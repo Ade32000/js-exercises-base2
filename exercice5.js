79bfa8040e13a72d659815a605cb69f21d7916da
Adeline <adeline.contact@gmail.com>
2018-06-15 10:15:27 +0200

    ex5 js base2

---
+++ exercice5.js
@@ -0,0 +1,44 @@
+document.getElementById('image1').onmouseover = function () 
+    {
+        document.getElementById('image1').setAttribute("src","exercice 5/images/image1_2.jpg");
+    }
+document.getElementById('image1').onmouseout = function () 
+    {
+        document.getElementById('image1').setAttribute("src","exercice 5/images/image1.jpg");
+    }
+
+document.getElementById('image2').onmouseover = function () 
+    {
+        document.getElementById('image2').setAttribute("src","exercice 5/images/image2_2.jpg");
+    }
+document.getElementById('image2').onmouseout = function () 
+    {
+        document.getElementById('image2').setAttribute("src","exercice 5/images/image2.jpg");
+    }
+
+document.getElementById('image3').onmouseover = function () 
+    {
+        document.getElementById('image3').setAttribute("src","exercice 5/images/image3_2.jpg");
+    }
+document.getElementById('image3').onmouseout = function () 
+    {
+        document.getElementById('image3').setAttribute("src","exercice 5/images/image3.jpg");
+    }
+
+document.getElementById('image4').onmouseover = function () 
+    {
+        document.getElementById('image4').setAttribute("src","exercice 5/images/image4_2.jpg");
+    }
+document.getElementById('image4').onmouseout = function () 
+    {
+        document.getElementById('image4').setAttribute("src","exercice 5/images/image4.jpg");
+    }
+
+document.getElementById('image5').onmouseover = function () 
+    {
+        document.getElementById('image5').setAttribute("src","exercice 5/images/image5_2.jpg");
+    }
+document.getElementById('image5').onmouseout = function () 
+    {
+        document.getElementById('image5').setAttribute("src","exercice 5/images/image5.jpg");
+    }

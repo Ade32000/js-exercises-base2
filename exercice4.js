7e26612e591f1c49775b666ce093ba4df2004f21
Adeline <adeline.contact@gmail.com>
2018-06-15 09:39:09 +0200

    ex4 js base2

---
+++ exercice4.js
@@ -0,0 +1,6 @@
+document.getElementById('clear').onclick = function () 
+    {
+     document.getElementById("nom").value = " ";
+     document.getElementById("prenom").value = " ";
+     document.getElementById("ville").value = " ";   
+    }

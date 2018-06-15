d2e16d94f9fc89d678e54b249ef9d322c89c10d5
Adeline <adeline.contact@gmail.com>
2018-06-15 09:46:22 +0200

    correction ex1 js base2

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,10 @@
 document.getElementById('image1').onmouseover = function () 
     {
-        document.getElementById('image1').setAttribute("src","images/image1_2.jpg");
+        document.getElementById('image1').setAttribute("src","exercice 1/images/image1_2.jpg");
     }
 document.getElementById('image1').onmouseout = function () 
     {
-        document.getElementById('image1').setAttribute("src","images/image1.jpg");
+        document.getElementById('image1').setAttribute("src","exercice 1/images/image1.jpg");
     }
+
+    
